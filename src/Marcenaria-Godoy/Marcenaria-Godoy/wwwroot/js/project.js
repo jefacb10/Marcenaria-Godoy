0a008fd024657e30bc2db61a36bf6031a41d4423
Jeferson A. C. Barros <32496187+jefacb10@users.noreply.github.com>
2022-10-23 05:38:27 -0300

    Inclusão de javascript

---
+++ src/Marcenaria-Godoy/Marcenaria-Godoy/wwwroot/js/project.js
@@ -0,0 +1,21 @@
+﻿
+let imagens = document.querySelectorAll('.small-img');
+let modal = document.querySelector('.modal-img');
+let modalImg = document.querySelector('#modal-imagem');
+let btnClose = document.querySelector('#btn-close');
+let srcVal = "";
+
+for (let i = 0; i < imagens.length; i++) {
+    imagens[i].addEventListener('click', function () {
+        srcVal = imagens[i].getAttribute('src');
+        modalImg.setAttribute("src", srcVal);
+        modal.classList.toggle('modal_active');
+
+    });
+}
+
+btnClose.addEventListener('click', function () {
+    modal.classList.toggle('modal_active');
+});
+
+
